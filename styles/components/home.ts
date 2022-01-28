32e1b271545b87b085b224842cabf7b1e1200b55
João Lima <jg.limamarinho202@gmail.com>
2022-01-27 22:02:39 -0300

    Pack v2

---
+++ styles/components/home.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const HomeCointainer = styled.div`
+  * {
+    padding: 0;
+    margin: 0;
+  }
+  main {
+    width: 100vw;
+    height: 100vh;
+    background-color: #181820;
+  }
+  nav {
+    width: 100vw;
+    height: 70px;
+    background-color: #21212b;
+    box-shadow: 0px 5px 5px rgba(0, 0, 0, 0.4);
+  }
+`;
